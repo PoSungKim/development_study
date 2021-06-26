c2157cfedcef44aacd134f031fa0e834ec9b13ce
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-06-27 00:03:27 +0900

    Create 02.Method.java

---
+++ Spring/Java/docs.oracle.com/02.Method.java
@@ -0,0 +1,15 @@
+public final class Method 
+  extends AccessibleObject
+  implements GenericDeclaration, Member {
+  /*
+  A Method provides information about, and access to, a single method on a class or interface. The reflected method may be a class method or an instance method (including an abstract method).
+  A Method permits widening conversions to occur when matching the actual parameters to invoke with the underlying method's formal parameters, but it throws an IllegalArgumentException if a narrowing conversion would occur.
+  */
+
+  String      getDeclaringClass()
+  String	    getName()
+  Class<?>[]	getParameterTypes()
+  Class<?>	  getReturnType()
+  String	    toGenericString()
+  String	    toString()
+}
