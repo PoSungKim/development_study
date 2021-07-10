d49bb59ec855a0ed53f620c6d1c94bc82a44b569
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-07-10 11:33:37 +0900

    Update 09. ArrayDeque.java

--- Spring/Java/docs.oracle.com/09. ArrayDeque.java
+++ Spring/Java/docs.oracle.com/09. ArrayDeque.java
@@ -5,7 +5,13 @@ public class ArrayDeque<E>
   implements Deque<E>, Cloneable, Serializable {
   
   /*
-  Resizable-array implementation of the Deque interface. Array deques have no capacity restrictions; they grow as necessary to support usage. They are not thread-safe; in the absence of external synchronization, they do not support concurrent access by multiple threads. Null elements are prohibited. 
+  Resizable-array implementation of the Deque interface. 
+  Array deques have no capacity restrictions; 
+  they grow as necessary to support usage. 
+  
+  They are not thread-safe; 
+  in the absence of external synchronization, they do not support concurrent access by multiple threads. 
+  Null elements are prohibited. 
 
   This class is likely to be faster than Stack when used as a stack, and faster than LinkedList when used as a queue.
   */
