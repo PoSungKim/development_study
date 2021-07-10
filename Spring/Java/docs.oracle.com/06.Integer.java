29fb0068b0a99a8458e338eaf4d9e0b732cfdd42
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-07-10 10:32:56 +0900

    Create 06.Integer.java

---
+++ Spring/Java/docs.oracle.com/06.Integer.java
@@ -0,0 +1,13 @@
+// https://docs.oracle.com/javase/7/docs/api/java/lang/Integer.html
+
+public final class Integer
+  extends Number
+  implements Comparable<Integer> {
+  
+  /*
+  The Integer class wraps a value of the primitive type int in an object. An object of type Integer contains a single field whose type is int.
+  In addition, this class provides several methods for converting an int to a String and a String to an int, as well as other constants and methods useful when dealing with an int.  */
+  
+  static String	toString(int i);
+  static int	  parseInt(String s);
+}
