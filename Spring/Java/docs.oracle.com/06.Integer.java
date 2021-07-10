155ccb4e6b995c3df455085ef4dbcf421b7f1237
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-07-10 10:35:38 +0900

    Update 06.Integer.java

--- Spring/Java/docs.oracle.com/06.Integer.java
+++ Spring/Java/docs.oracle.com/06.Integer.java
@@ -8,6 +8,6 @@ public final class Integer
   The Integer class wraps a value of the primitive type int in an object. An object of type Integer contains a single field whose type is int.
   In addition, this class provides several methods for converting an int to a String and a String to an int, as well as other constants and methods useful when dealing with an int.  */
   
-  static String	toString(int i);
-  static int	  parseInt(String s);
+  static String toString(int i);
+  static int    parseInt(String s);
 }
