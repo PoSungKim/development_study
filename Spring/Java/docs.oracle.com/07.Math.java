0ae70296aa0174a53a87a718a8196dbd60424a7b
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-07-10 10:35:16 +0900

    Create 07.Math.java

---
+++ Spring/Java/docs.oracle.com/07.Math.java
@@ -0,0 +1,12 @@
+// https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html
+
+public final class Math
+  extends Object {
+  
+  /*
+  The class Math contains methods for performing basic numeric operations such as the elementary exponential, logarithm, square root, and trigonometric functions.
+  */
+  
+  static int    max(int a, int b); // double, float, int, long
+  static int    min(int a, int b); // double, float, int, long
+}
