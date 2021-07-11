56db4d163ca65c770fdbbed6f4f32b53aa6f51dd
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-07-11 17:13:15 +0900

    Update 13.HashMap.java

--- Spring/Java/docs.oracle.com/13.HashMap.java
+++ Spring/Java/docs.oracle.com/13.HashMap.java
@@ -1,1 +1,25 @@
+// https://docs.oracle.com/javase/8/docs/api/java/util/HashMap.html
 
+public class HashMap<K,V>
+  extends AbstractMap<K,V>
+  implements Map<K,V>, Cloneable, Serializable {
+
+  void      clear();
+  Object    clone();
+  
+  boolean   containsKey(Object key);
+  boolean   containsValue(Object value);
+  
+  V         get(Object key);
+  V         getOrDefault(Object key, V defaultValue);
+  
+  boolean   isEmpty();
+  
+  V         put(K key, V value);
+  void      putAll(Map<? extends K,? extends V> m);
+  
+  V         remove(Object key);
+  boolean   remove(Object key, Object value);
+  
+  int       size();
+}
