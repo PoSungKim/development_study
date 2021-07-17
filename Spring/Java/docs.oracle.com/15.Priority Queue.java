b076c59b58f017e46f8da5257cfbebad4fbe7c65
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-07-17 22:47:48 +0900

    Create 15.Priority Queue.java

---
+++ Spring/Java/docs.oracle.com/15.Priority Queue.java
@@ -0,0 +1,34 @@
+// https://docs.oracle.com/javase/7/docs/api/java/util/PriorityQueue.html
+
+public class PriorityQueue<E>
+  extends AbstractQueue<E>
+  implements Serializable {
+  // An unbounded priority queue based on a priority heap. 
+  
+  // The elements of the priority queue are ordered according to their natural ordering, 
+  // or by a Comparator provided at queue construction time, depending on which constructor is used. 
+  
+  // Constructor
+  PriorityQueue();
+  PriorityQueue(Collection<? extends E> c);
+  PriorityQueue(int initialCapacity);
+  PriorityQueue(int initialCapacity, Comparator<? super E> comparator);
+  PriorityQueue(PriorityQueue<? extends E> c);
+  PriorityQueue(SortedSet<? extends E> c);
+  
+  // Methods
+  boolean	add(E e);
+  boolean	offer(E e);
+  E	peek();
+  E	poll();
+  boolean	remove(Object o);
+  boolean	contains(Object o);
+
+  int	size();
+  void	clear();
+  Object[]	toArray();
+  <T> T[]	toArray(T[] a);
+
+  Comparator<? super E>	comparator();
+  Iterator<E>	iterator();
+}
