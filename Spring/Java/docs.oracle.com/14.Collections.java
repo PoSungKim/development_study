5f9c56a87be158243de59d8ac9cf071430ff3fd2
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-07-17 18:34:59 +0900

    Update 14.Collections.java

--- Spring/Java/docs.oracle.com/14.Collections.java
+++ Spring/Java/docs.oracle.com/14.Collections.java
@@ -5,7 +5,7 @@ public class Collections
   static <T> int binarySearch(List<? extends Comparable<? super T>> list, T key);
   static <T> int binarySearch(List<? extends T> list, T key, Comparator<? super T> c);
   
-  static <T extends Comparable<? super T>> void	sort(List<T> list);
-  static <T> void	sort(List<T> list, Comparator<? super T> c)
+  static <T extends Comparable<? super T>> void sort(List<T> list);
+  static <T> void sort(List<T> list, Comparator<? super T> c)
 }
 
