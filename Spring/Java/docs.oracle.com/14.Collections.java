ecf1d22d00a9d9efc44839800606f4073c8ff903
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-07-17 18:39:33 +0900

    Update 14.Collections.java

--- Spring/Java/docs.oracle.com/14.Collections.java
+++ Spring/Java/docs.oracle.com/14.Collections.java
@@ -4,8 +4,9 @@ public class Collections
   extends Object {
   static <T> int binarySearch(List<? extends Comparable<? super T>> list, T key);
   static <T> int binarySearch(List<? extends T> list, T key, Comparator<? super T> c);
+  // the index of the search key, if it is contained in the list; otherwise, (-(insertion point) - 1)
+  // if return value < 0, then -(return value+1); becomes the insertion point 
   
   static <T extends Comparable<? super T>> void sort(List<T> list);
   static <T> void sort(List<T> list, Comparator<? super T> c)
 }
-
