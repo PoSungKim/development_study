4f08167baf2cf3f214a548be5fd5aacd13070d3d
60029756 <a60029756@MacBook-Pro.local>
2021-10-07 11:52:30 +0900

    기존 Shell 폴더 삭제

--- Shell/Compiler/importJava.java
+++
@@ -1,9 +0,0 @@
-import CompileTest.compileJava;
-
-public class importJava {
-    public static void main(String[] args) {
-        System.out.println("Import Java...");
-        compileJava CJ = new compileJava();
-        CJ.main(args);
-    }
-}
